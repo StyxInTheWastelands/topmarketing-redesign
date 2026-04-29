bd97a884d3c6452e1de7820399eaf7d5e9fb0ce8
StyxInTheWastelands <sahsakovalenko38@gmail.com>
2026-04-29 20:25:41 +0200

    fix:added basename to router for gh-pages compatibility

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import { Layout } from "./components/Layout";
-import { LanguageProvider } from "./components/LanguageContext"; // Import this!
+import { LanguageProvider } from "./components/LanguageContext";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
@@ -12,25 +12,30 @@ import { PrintPromo } from "./pages/oferta/PrintPromo";
 import { PromotionalSets } from "./pages/oferta/PromotionalSets";
 import { Confection } from "./pages/oferta/Confection";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <LanguageProvider>
+          <Layout />
+        </LanguageProvider>
+      ),
+      children: [
+        { index: true, Component: Home },
+        { path: "o-nas", Component: About },
+        { path: "kontakt", Component: Contact },
+        { path: "katalogi", Component: Catalogs },
+        { path: "oferta/zestawy-upominkowe", Component: GiftSets },
+        { path: "oferta/slodycze-reklamowe", Component: PromotionalSweets },
+        { path: "oferta/opakowania", Component: Packaging },
+        { path: "oferta/materialy-poligraficzne", Component: PrintPromo },
+        { path: "oferta/sety-reklamowe", Component: PromotionalSets },
+        { path: "oferta/konfekcjonowanie", Component: Confection },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: (
-      <LanguageProvider>
-        <Layout />
-      </LanguageProvider>
-    ),
-    children: [
-      { index: true, Component: Home },
-      { path: "o-nas", Component: About },
-      { path: "kontakt", Component: Contact },
-      { path: "katalogi", Component: Catalogs },
-      { path: "oferta/zestawy-upominkowe", Component: GiftSets },
-      { path: "oferta/slodycze-reklamowe", Component: PromotionalSweets },
-      { path: "oferta/opakowania", Component: Packaging },
-      { path: "oferta/materialy-poligraficzne", Component: PrintPromo },
-      { path: "oferta/sety-reklamowe", Component: PromotionalSets },
-      { path: "oferta/konfekcjonowanie", Component: Confection },
-    ],
-  },
-]);
+    basename: "/topmarketing-redesign",
+  }
+);
